6a93f5b7198c778890edb3a4544dc0e76655e969
Rafael Alves <84939473+rafaelalvesmds@users.noreply.github.com>
2022-05-16 15:21:30 -0300

    responsive table

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+
+export interface PeriodicElement {
+  coluna1: string;
+  coluna2: string;
+  coluna3: string;
+  coluna4: string;
+  coluna5: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { coluna1: 'Conteúdo', coluna2: 'Conteúdo', coluna3: 'Conteúdo', coluna4: 'Conteúdo', coluna5: 'Conteúdo' },
+];
 
 @Component({
   selector: 'app-home',
@@ -7,9 +19,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  columns: string[] = ['coluna1', 'coluna2', 'coluna3', 'coluna4', 'coluna5'];
+  dataSource = ELEMENT_DATA;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.getScreenWidth = window.innerWidth;
   }
+
+  getScreenWidth!: number;
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.getScreenWidth = window.innerWidth;
+  }
+
 
 }
